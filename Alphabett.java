bb51e4d1c36a952e4c3e61bfd01958da2a1e9118
Sowndharya13197 <sowndharyagopal13@gmail.com>
2017-06-05 07:45:30 -0700

    Create Alphabett.java

---
+++ Alphabett.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Alphabet {
+    public static void main(String args[])
+    {System.out.println("entr char");
+    Scanner s=new Scanner(System.in);
+    char n=s.next().charAt(0);
+    if(n>='a'&& n<='z')
+    {System.out.println("alphabet");
+    }
+    else
+    {System.out.println("not alphabet");
+    
+}}}
